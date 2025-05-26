74ee17baf8494b941065a43b894221a801ddd4bf
TWTwoFace <pipo2017@mail.ru>
2025-05-26 06:30:27 +0300

    feat: 4 lab work done: '

--- docs/PostgreSQL/db_lab2_popov.sql
+++ docs/PostgreSQL/db_lab2_popov.sql
@@ -1,103 +1,100 @@
-CREATE TABLE "user" (
-  "id" integer PRIMARY KEY,
-  "nickname" varchar UNIQUE,
-  "login" varchar UNIQUE,
-  "password_hash" varchar,
-  "money" integer,
-  "clan_id" integer
+CREATE TABLE clans (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(20) UNIQUE,
+  description VARCHAR(100),
+  avatar INT
 );
 
-CREATE TABLE "statistics" (
-  "id" integer PRIMARY KEY,
-  "user_id" integer,
-  "win_count" integer,
-  "matches_count" integer,
-  "max_rating" integer,
-  "current_rating" integer
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  nickname VARCHAR(20) UNIQUE NOT NULL,
+  login VARCHAR(20) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  money INT NOT NULL DEFAULT 1000,
+  clan_id INT DEFAULT NULL,
+  FOREIGN KEY (clan_id) REFERENCES clans (id) ON DELETE SET NULL
 );
 
-CREATE TABLE "clan" (
-  "id" integer PRIMARY KEY,
-  "name" varchar UNIQUE,
-  "description" varchar,
-  "avatar" integer
+ALTER TABLE clans
+  ADD COLUMN owner_id INT UNIQUE NOT NULL;
+
+ALTER TABLE clans
+  ADD CONSTRAINT owner_id_fkey FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE;
+
+
+CREATE TABLE statistics (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  win_count INT NOT NULL DEFAULT 0,
+  matches_count INT NOT NULL DEFAULT 0,
+  max_rating INT NOT NULL DEFAULT 100,
+  current_rating INT NOT NULL DEFAULT 100,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "clan_action" (
-  "id" integer PRIMARY KEY,
-  "user_id" integer,
-  "clan_id" integer,
-  "description" varchar
+
+CREATE TABLE clan_actions (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  clan_id INT NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (clan_id) REFERENCES clans(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "match_statistics" (
-  "id" integer PRIMARY KEY,
-  "host_id" integer,
-  "peer_id" integer,
-  "result" integer,
-  "duration" integer
+CREATE TABLE match_statistics (
+  id SERIAL PRIMARY KEY,
+  host_id INT NOT NULL,
+  peer_id INT NOT NULL,
+  result INT NOT NULL,
+  duration INT NOT NULL,
+  room_id INT NOT NULL,
+  FOREIGN KEY (host_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (peer_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "matchmakers_room" (
-  "id" integer PRIMARY KEY,
-  "host_id" integer,
-  "rating" integer,
-  "state" boolean
+CREATE TABLE matchmakers_rooms (
+  id SERIAL PRIMARY KEY,
+  host_id INT UNIQUE NOT NULL,
+  peer_id INT UNIQUE DEFAULT NULL,
+  state BOOLEAN NOT NULL DEFAULT false,
+  FOREIGN KEY (host_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (peer_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
-CREATE TABLE "deck" (
-  "id" integer PRIMARY KEY,
-  "deck_number" integer,
-  "user_id" integer
+CREATE TABLE decks (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  deck_number INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "card" (
-  "id" integer PRIMARY KEY,
-  "card_id" integer,
-  "user_id" integer,
-  "deck_id" integer,
-  "level" integer,
-  "power" float
+CREATE TABLE cards (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,  
+  deck_id INT DEFAULT NULL,
+  card_id INT NOT NULL,
+  level INT NOT NULL DEFAULT 1,
+  power FLOAT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (deck_id) REFERENCES decks(id) ON DELETE SET NULL
 );
 
-CREATE TABLE "shop" (
-  "id" integer PRIMARY KEY,
-  "update_date" timestamp
+CREATE TABLE shops (
+  id SERIAL PRIMARY KEY,
+  update_date timestamp NOT NULL
 );
 
-CREATE TABLE "sell_item" (
-  "id" integer PRIMARY KEY,
-  "card_id" integer,
-  "cost" integer
+CREATE TABLE sell_items (
+  id SERIAL PRIMARY KEY,
+  card_id INT NOT NULL,
+  cost INT NOT NULL
 );
 
-ALTER TABLE "card" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "deck" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "statistics" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "match_statistics" ADD FOREIGN KEY ("host_id") REFERENCES "user" ("id");
-
-ALTER TABLE "match_statistics" ADD FOREIGN KEY ("peer_id") REFERENCES "user" ("id");
-
-ALTER TABLE "matchmakers_room" ADD FOREIGN KEY ("host_id") REFERENCES "user" ("id");
-
-ALTER TABLE "clan_action" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("clan_id") REFERENCES "clan" ("id");
-
-ALTER TABLE "clan_action" ADD FOREIGN KEY ("clan_id") REFERENCES "clan" ("id");
-
-ALTER TABLE "card" ADD FOREIGN KEY ("deck_id") REFERENCES "deck" ("id");
-
-CREATE TABLE "shop_sell_item" (
-  "shop_id" integer,
-  "sell_item_id" integer,
-  PRIMARY KEY ("shop_id", "sell_item_id")
+CREATE TABLE shops_sell_items(
+  id SERIAL PRIMARY KEY,
+  shop_id INT NOT NULL,
+  sell_item_id INT NOT NULL,
+  FOREIGN KEY (shop_id) REFERENCES shops(id) ON DELETE CASCADE,
+  FOREIGN KEY (sell_item_id) REFERENCES sell_items(id) ON DELETE CASCADE
 );
-
-ALTER TABLE "shop_sell_item" ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id");
-
-ALTER TABLE "shop_sell_item" ADD FOREIGN KEY ("sell_item_id") REFERENCES "sell_item" ("id");
-
